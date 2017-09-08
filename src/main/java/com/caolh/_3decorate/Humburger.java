e8a278c393326e8501103a2fb64eda35514ae20b
caolh <545018520@qq.com>
2017-09-08 21:30:55 +0800

    装饰设计模式

---
+++ src/main/java/com/caolh/_3decorate/Humburger.java
@@ -0,0 +1,18 @@
+package com.caolh._3decorate;
+
+/**
+ * Create with IntelliJ IDEA
+ * User:曹立华
+ * Date:2017/9/8
+ */
+public abstract class Humburger {
+
+    protected  String name ;
+
+    public String getName(){
+        return name;
+    }
+
+    public abstract double getPrice();
+
+}
---
+++ src/main/java/com/caolh/_3decorate/impl/Chilli.java
@@ -0,0 +1,32 @@
+package com.caolh._3decorate.impl;
+
+import com.caolh._3decorate.Condiment;
+import com.caolh._3decorate.Humburger;
+
+/**
+ * Create with IntelliJ IDEA
+ * User:曹立华
+ * Date:2017/9/8
+ */
+//辣椒调料
+public class Chilli extends Condiment {
+
+    Humburger humburger;
+
+    public Chilli(Humburger humburger){
+        this.humburger = humburger;
+
+    }
+
+    @Override
+    public String getName() {
+        return humburger.getName()+" 加辣椒";
+    }
+
+    @Override
+    public double getPrice() {
+        return humburger.getPrice();  //辣椒是免费的哦
+    }
+
+}
+
